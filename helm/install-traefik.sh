b8dade57174e0c40cc0c95142c60cb5ef1cd9fb0
cuilan <419475937@qq.com>
2025-05-07 18:08:27 +0800

    auto commit and push by cuilan on 2025-05-07 18:08:27

--- helm/install-traefik.sh
+++ helm/install-traefik.sh
@@ -4,6 +4,16 @@ helm repo add traefik https://traefik.github.io/charts
 
 helm repo update
 
-helm install traefik traefik/traefik -n kube-addons --create-namespace -f traefik.values.yaml
+helm install traefik traefik/traefik \
+  -n kube-addons \
+  --create-namespace \
+  -f traefik.values.yaml
+
+helm install traefik traefik/traefik \
+  -n kube-addons \
+  --create-namespace \
+  --set image.pullPolicy=IfNotPresent \
+  --set image.tag=v3.1.2 \
+  -f traefik.values.yaml
 
 helm uninstall traefik -n kube-addons
